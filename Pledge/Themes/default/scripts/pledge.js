c87b32acb120e45f3c7e7a9caa3be6742e3391a8
Mick <100359251+Mickjav@users.noreply.github.com>
2023-07-27 21:55:45 +0100

    Delete Pledge directory

--- Pledge/Themes/default/scripts/pledge.js
+++
@@ -1,9 +0,0 @@
-function show_details($ID) {
-	var x = document.getElementById($ID);
-	if (x.style.display === "none") {
-		x.style.display = "table-cell";
-	}
-	else {
-		x.style.display = "none";
-	}
-}
